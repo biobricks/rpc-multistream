821a289dc9fe848303d8cf26cd769e6e62a2addd
Yardena Cohen <yardenack@gmail.com>
2017-05-12 16:05:40 -0700

    explicit.js: test with explicit off
    not sure if i'm doing this right, but it seems to work

--- tests/explicit.js
+++ tests/explicit.js
@@ -42,3 +42,39 @@ test('opts.explicit.on', function (t) {
         });
     })
 })
+test('opts.explicit.off', function (t) {
+    var server = rpc({
+        foo: rpc.syncReadStream(function() {
+            return fs.createReadStream('tests/foo.txt', {encoding: 'utf8'});
+        }),
+        bar: rpc.syncReadStream(function() {
+            var i = 0;
+            return from.obj(function(size, next) {
+                if(i++) return next(null, null);
+                next(null, {
+                    hoopy: 'frood'
+                });
+            });
+        }, { objectMode: true }) // bar is objectMode
+    }, { objectMode: false }); // foo is not
+    t.plan(3)
+    var client = rpc(undefined, {
+        objectMode: true, // different default than the server
+        explicit: false // this causes errors, now we have to catch them
+    });
+    client.pipe(server).pipe(client);
+    client.on('methods', function(methods) {
+        t.equal(typeof methods.foo, 'function', 'explicit client: methods.foo is a function')
+        t.equal(typeof methods.bar, 'function', 'explicit client: methods.bar is a function')
+
+        methods.foo().on('error', function(err) {
+            t.equal(err.message,"Could not parse row I am the contents of foo.txt :)...","stringstream error")
+        })
+
+        var objStream = methods.bar();
+        objStream.on('data', function(data) {
+            t.equal(typeof data,"object","explicit client: bar gives an object")
+            t.deepEqual(data,{hoopy: 'frood'},"explicit client: bar object is correct")
+        });
+    })
+})
